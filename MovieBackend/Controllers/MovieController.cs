35abb21fe9a0e99b3a867fadcab2bd85a8bfe933
favel <favel@DESKTOP-OUVCHLN>
2023-12-13 11:56:37 -0600

    Add project files.

---
+++ MovieBackend/Controllers/MovieController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<MovieController> _logger;
+
+        public MovieController(ILogger<MovieController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetMovies")]
+        public IEnumerable<Movie> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Movie
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet(Name = "GetMovie")]
+        public Movie GetMovie(int id)
+        {
+
+        }
+
+        [HttpPost]
+    }
+}
---
+++ MovieBackend/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Movie		
+{
+	public int Id { get; set; }
+
+	public string Title;
+
+	public string Genre;
+	public DateOnly ReleaseDate { get; set; }
+
+}
