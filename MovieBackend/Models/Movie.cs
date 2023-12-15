0d575a54b10554067b4062d76dc17c69f94e7d5b
jfavelle <jfavelle@hotmail.com>
2023-12-15 10:03:40 -0600

    Added Required to ID and DB Schema

--- MovieBackend/Models/Movie.cs
+++ MovieBackend/Models/Movie.cs
@@ -2,6 +2,18 @@
 
 public class Movie		
 {
+    /*Database Schema would follow this
+	CREATE TABLE movies (
+        Id int NOT NULL AUTO_INCREMENT,
+		Title varchar(1000) NOT NULL,
+		Genre varchar(300) NOT NULL,
+		ReleaseDate DATE,
+		IMDBRating FLOAT,
+		PRIMARY KEY(Id)
+	);
+	CREATE INDEX idxTitle ON Title;
+	 */
+    [Required]
 	public int Id { get; set; }
 
 	[Required]
